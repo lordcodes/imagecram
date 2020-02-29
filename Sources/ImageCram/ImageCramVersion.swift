df8fdf2b530c9249ade8c13dd6c172ece3f67309
Andrew Lord <andrewlord1990@gmail.com>
2020-02-29 21:14:47 +0000

    Version argument

---
+++ Sources/ImageCram/ImageCramVersion.swift
@@ -0,0 +1,7 @@
+// Copyright (C) 2020 Andrew Lord
+
+public class ImageCramVersion {
+    private init() {}
+
+    public static let current = "0.0.1"
+}
--- Sources/ImageCramCLI/ImageCramCommand.swift
+++ Sources/ImageCramCLI/ImageCramCommand.swift
@@ -3,17 +3,27 @@
 import ArgumentParser
 import Files
 import Foundation
+import ImageCram
 
 struct ImageCramCommand: ParsableCommand {
+    static let configuration = CommandConfiguration(commandName: "imagecram")
+
+    @Argument(help: "The input image file or directory of images")
+    var input: String?
+
     @Flag(name: .shortAndLong, help: "Output the version number")
     var version: Bool
 
     @Option(name: .shortAndLong, help: "Output file or directyory")
-    var output: String
+    var output: String?
 
     @Flag(name: .shortAndLong, help: "Silence any output except errors")
     var quiet: Bool
 
-    @Argument(help: "The input image file or directory of images")
-    var input: String
+    func run() throws {
+        if version {
+            print(ImageCramVersion.current)
+            return
+        }
+    }
 }
